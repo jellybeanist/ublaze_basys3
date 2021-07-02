66052be20fa6ce4b1eb5cabb2f5419b723506ff1
Burak Can KARAKURT <84005554+jellybeanist@users.noreply.github.com>
2021-07-02 10:32:33 +0300

    Update data_types.vhd
    Switch addition.

--- data_types.vhd
+++ data_types.vhd
@@ -5,7 +5,7 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 USE STD.TEXTIO.ALL;
 
 package data_types is
-
+    -- CPU -->> PL
     type CONTROL_REGISTERS is 
     record
         --LEDS
@@ -25,6 +25,28 @@ package data_types is
         ext_led_13              : std_logic_vector(31 downto 0);
         ext_led_14              : std_logic_vector(31 downto 0);
         ext_led_15              : std_logic_vector(31 downto 0);
+
+    end record;
+    -- PL -->> CPU    
+    type STAT_REGISTERS is 
+    record
+        --SWITCHES        
+        ext_sw_00               : std_logic_vector(31 downto 0);
+        ext_sw_01               : std_logic_vector(31 downto 0);
+        ext_sw_02               : std_logic_vector(31 downto 0);
+        ext_sw_03               : std_logic_vector(31 downto 0);
+        ext_sw_04               : std_logic_vector(31 downto 0);
+        ext_sw_05               : std_logic_vector(31 downto 0);
+        ext_sw_06               : std_logic_vector(31 downto 0);
+        ext_sw_07               : std_logic_vector(31 downto 0);
+        ext_sw_08               : std_logic_vector(31 downto 0);
+        ext_sw_09               : std_logic_vector(31 downto 0);
+        ext_sw_10               : std_logic_vector(31 downto 0);
+        ext_sw_11               : std_logic_vector(31 downto 0);
+        ext_sw_12               : std_logic_vector(31 downto 0);
+        ext_sw_13               : std_logic_vector(31 downto 0);
+        ext_sw_14               : std_logic_vector(31 downto 0);
+        ext_sw_15               : std_logic_vector(31 downto 0);
         
     end record;
 end data_types;
