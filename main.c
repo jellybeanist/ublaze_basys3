5e370fc2c3f9480af9af9caa6ea5aba008941a90
Burak Can KARAKURT <84005554+jellybeanist@users.noreply.github.com>
2021-07-02 10:33:14 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,20 @@
-
 #include "main.h"
 #include <stdio.h>
 #include <sleep.h>
-u32 HIGH = 1;
-u32 LOW = 0;
 u32 address = 0;
 
 int main (void)
 {
 	while (1)
 	{
-		for (int i=0; i<16; i = i++)
+
+		//switches are being used to change value (HIGH or LOW) of the LEDs, controlled by registers.
+		for (int i=0; i<16; i++)
 		{
-			((u32*)(BRAM_WRITE_BASE_ADDRESS + (address+i)))[0] = HIGH;
+			((u32*)(BRAM_WRITE_BASE_ADDRESS + 4*(address+i)))[0] = ((u32*)(BRAM_READ_BASE_ADDRESS + 4*(address+i)))[0];
 		}
-		sleep(2);
-		for (int i=0; i<16; i = i++)
-		{
-			((u32*)(BRAM_WRITE_BASE_ADDRESS + (address+i)))[0] = LOW;
-		}
-		sleep(2);
+
 	}
+
 	return 0;
 }
